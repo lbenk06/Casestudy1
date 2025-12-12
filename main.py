fcc87ab8a1dd5a5127c32dbd5ddcca4f53eed73b
lbenk06 <bl8299ci4me.at>
2025-12-12 11:36:16 +0100

    main geändert

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello Worhhhld")
+import streamlit as st
+
+# Eine Überschrift der ersten Ebene
+st.write("# Gerätemanagement")
+
+# Eine Überschrift der zweiten Ebene
+st.write("## Geräteauswahl")
+
+# Eine Auswahlbox mit hard-gecoded Optionen, das Ergebnis
+
+current_device = st.selectbox(label='Gerät auswählen',
+        options = ["3d-Drucker", ""])
+
+st.write(f"Das ausgewählte Gerät ist {current_device}")
